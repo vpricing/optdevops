e3538e508e613e9a897fe1da45cb5974cfcb7aaa
Luis Gerardo Calderon Contreras <luis.calderon@volaris.com>
2023-05-03 12:50:21 -0600

    Add envars and optsecuritystack

---
+++ envvars.ps1
@@ -0,0 +1,28 @@
+## Environment Variables
+
+#Profile Environment
+$DevAccount=''
+$DevProfile=''
+$UatAccount=''
+$UatProfile=''
+$ProdAccount=''
+$ProdProfile=''
+$DevOpsAccount=''
+$DevOpsProfile=''
+
+## Network Parameters
+# CIDR
+$DevNavPrivateCIDR='/16'
+$DevNavPublicCIDR='/16'
+$DevOptProjectCIDR='/16'
+$UatNavPrivateCIDR='/16'
+$UatNavPublicCIDR='/16'
+$UatOptProjectCIDR='/16'
+$ProdNavPrivateCIDR='/16'
+$ProdNavPublicCIDR='/16'
+$ProdOptProjectCIDR='/16'
+
+# Transit Gateway Accepter
+$TGWNetAccount=''
+$TGWID=''
+$TGRegion='us-east-1'
