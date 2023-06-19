8c144dc0e55727c6b7f6110f4da063420e513548
Luis Gerardo Calderon Contreras <luis.calderon@volaris.com>
2023-06-19 12:47:00 -0600

    tags and envvars

--- envvars.ps1
+++ envvars.ps1
@@ -30,14 +30,14 @@ $TGRegion='us-east-1'
 $devAdmRoleCred = (aws sts assume-role --role-arn "arn:aws:iam::108160666657:role/rm/opt/usr-role/opt.CloudMngAccess" --role-session-name "AWSCLI-Session-LGCC-admin" --profile $DevProfile | ConvertFrom-Json).Credentials
 $env:AWS_ACCESS_KEY_ID=$devAdmRoleCred.AccessKeyId
 $env:AWS_SECRET_ACCESS_KEY=$devAdmRoleCred.SecretAccessKey
-$env:AWS_DEFAULT_REGION="us-east-2"
+$env:AWS_DEFAULT_REGION="us-east-1"
 $env:AWS_SESSION_TOKEN=$devAdmRoleCred.SessionToken
 
 aws cloudformation create-stack --stack-name "opt-dev-security" --template-body file://opt-security-stack.yml --capabilities CAPABILITY_NAMED_IAM
 aws cloudformation update-stack --stack-name "opt-dev-security" --template-body file://opt-security-stack.yml --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation validate-template --template-body file://opt-network-stack.yml
-aws cloudformation create-stack --stack-name "opt-dev-network" --template-body file://opt-network-stack.yml --capabilities CAPABILITY_NAMED_IAM --parameter ParameterKey=TGRegion,ParameterValue=us-east-2 ParameterKey=TGatewayId,ParameterValue=tgw-08ef88e69dcd07c7a ParameterKey=TGAccountId,ParameterValue=072515348649 ParameterKey=NavPrivCIDR,ParameterValue="10.111.0.0/16" ParameterKey=NavPubCIDR,ParameterValue="10.112.0.0/16" ParameterKey=ProjectCIDR,ParameterValue='10.106.0.0/16'
+aws cloudformation create-stack --stack-name "vol-com-opt-network-devenv" --template-body file://opt-network-stack.yml --capabilities CAPABILITY_NAMED_IAM
 
-aws cloudformation update-stack --stack-name "opt-dev-network" --template-body file://opt-network-stack.yml --capabilities CAPABILITY_NAMED_IAM --parameter ParameterKey=TGRegion,ParameterValue=us-east-2 ParameterKey=TGatewayId,ParameterValue=tgw-08ef88e69dcd07c7a ParameterKey=TGAccountId,ParameterValue=072515348649 ParameterKey=NavPrivCIDR,ParameterValue="10.111.0.0/16" ParameterKey=NavPubCIDR,ParameterValue="10.112.0.0/16" ParameterKey=ProjectCIDR,ParameterValue='10.106.0.0/16'
-aws cloudformation delete-stack --stack-name "opt-dev-network"
+aws cloudformation update-stack --stack-name "vol-com-opt-network-devenv" --template-body file://opt-network-stack.yml --capabilities CAPABILITY_NAMED_IAM --parameter ParameterKey=TGRegion,ParameterValue=us-east-2 ParameterKey=TGatewayId,ParameterValue=tgw-08ef88e69dcd07c7a ParameterKey=TGAccountId,ParameterValue=072515348649 ParameterKey=NavPrivCIDR,ParameterValue="10.111.0.0/16" ParameterKey=NavPubCIDR,ParameterValue="10.112.0.0/16" ParameterKey=ProjectCIDR,ParameterValue='10.106.0.0/16'
+aws cloudformation delete-stack --stack-name "vol-com-opt-network-devenv"
